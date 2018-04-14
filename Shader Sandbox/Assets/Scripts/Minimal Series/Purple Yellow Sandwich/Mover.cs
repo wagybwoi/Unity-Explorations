c1b8133d246958704d1f7bff7f0bc0c8d66cc484
Ahmed Wageh <ahmedsamywageh@gmail.com>
2018-04-14 12:26:15 -0400

    Minimal scenes

---
+++ Shader Sandbox/Assets/Scripts/Minimal Series/Purple Yellow Sandwich/Mover.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	void Update () {
+		this.transform.position = new Vector3 (this.transform.position.x, -18f + Mathf.FloorToInt(Mathf.Sin (Time.realtimeSinceStartup/4f)*9f), this.transform.position.z);
+	}
+}
